ca109ab8cc0f643386404c066e6bc11514e76efd
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-26 14:31:58 +0100

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,8 +1,7 @@
 set -e
-CC=clang  CXX=clang++ CFLAGS=-fsanitize=address LIB_FUZZING_ENGINE=-engine=afl++
+CC=clang  CXX=clang++ CFLAGS=-fsanitize=address 
 export CC CXX
 set $LLVM_CONFIG=/usr/bin/llvm-config-10
-export LIB_FUZZING_ENGINE
 
 
 cd postfix
@@ -15,7 +14,7 @@ cd ${BASE}/src/global
 $CC $CFLAGS -DHAS_DEV_URANDOM -DSNAPSHOT -UUSE_DYNAMIC_LIBS -DDEF_SHLIB_DIR=\"no\" \
                -UUSE_DYNAMIC_MAPS -I. -I../../include -DNO_EAI -DDEF_SMTPUTF8_ENABLE=\"no\" \
                 -g -O -DLINUX4 -Wformat -Wno-comment -fno-common -c $SRC/fuzz_tok822.c
-$CC $CFLAGS $LIB_FUZZING_ENGINE -DHAS_DEV_URANDOM -DSNAPSHOT -UUSE_DYNAMIC_LIBS -DDEF_SHLIB_DIR=\"no\" \
+$CC $CFLAGS  -DHAS_DEV_URANDOM -DSNAPSHOT -UUSE_DYNAMIC_LIBS -DDEF_SHLIB_DIR=\"no\" \
                -UUSE_DYNAMIC_MAPS -I. -I../../include -DNO_EAI -DDEF_SMTPUTF8_ENABLE=\"no\" \
                 -g -O -DLINUX4 -Wformat -Wno-comment -fno-common -c $SRC/fuzz_mime.c
 
