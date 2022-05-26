7b59d3884ecd059fc6ebe63bb0dbb7a2a4312b13
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-26 12:04:07 +0100

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,6 +1,7 @@
-CC=clang
-CXX=clang++
+set -e
+CC=clang  CXX=clang++ CFLAGS=-fsanitize=address
 export CC CXX
+set $LLVM_CONFIG=/usr/bin/llvm-config-10
 cd postfix
 make makefiles CCARGS="${CFLAGS}"
 make
