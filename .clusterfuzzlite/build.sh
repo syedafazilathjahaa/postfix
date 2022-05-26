7171b2417968fd4e069a8fc1182b386e84578d4a
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-26 13:58:16 +0100

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,8 +1,8 @@
 set -e
-CC=clang  CXX=clang++ CFLAGS=-fsanitize=address FUZZING_ENGINE=afl
+CC=clang  CXX=clang++ CFLAGS=-fsanitize=address LIB_FUZZING_ENGINE=afl
 export CC CXX
 set $LLVM_CONFIG=/usr/bin/llvm-config-10
-export FUZZING_ENGINE=afl
+export LIB_FUZZING_ENGINE
 
 
 cd postfix
@@ -12,7 +12,7 @@ BASE=$PWD
 
 # Compile fuzzers
 cd ${BASE}/src/global
-$CC $CFLAGS $LIB_FUZZING_ENGINE -DHAS_DEV_URANDOM -DSNAPSHOT -UUSE_DYNAMIC_LIBS -DDEF_SHLIB_DIR=\"no\" \
+$CC $CFLAGS -DHAS_DEV_URANDOM -DSNAPSHOT -UUSE_DYNAMIC_LIBS -DDEF_SHLIB_DIR=\"no\" \
                -UUSE_DYNAMIC_MAPS -I. -I../../include -DNO_EAI -DDEF_SMTPUTF8_ENABLE=\"no\" \
                 -g -O -DLINUX4 -Wformat -Wno-comment -fno-common -c $SRC/fuzz_tok822.c
 $CC $CFLAGS $LIB_FUZZING_ENGINE -DHAS_DEV_URANDOM -DSNAPSHOT -UUSE_DYNAMIC_LIBS -DDEF_SHLIB_DIR=\"no\" \
@@ -22,7 +22,7 @@ $CC $CFLAGS $LIB_FUZZING_ENGINE -DHAS_DEV_URANDOM -DSNAPSHOT -UUSE_DYNAMIC_LIBS 
 
 # Link fuzzers
 cd ${BASE}
-$CC $CFLAGS $LIB_FUZZING_ENGINE ./src/global/fuzz_tok822.o -o $OUT/fuzz_tok822 \
+$CC $CFLAGS $LIB_FUZZING_ENGINE afl ./src/global/fuzz_tok822.o -o $OUT/fuzz_tok822 \
   ./lib/libglobal.a ./lib/libutil.a
 $CC $CFLAGS $LIB_FUZZING_ENGINE ./src/global/fuzz_mime.o -o $OUT/fuzz_mime \
   ./lib/libglobal.a ./lib/libutil.a -ldb -lnsl
