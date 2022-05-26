a268c9cee75a5725e5fec8cf5140e1f4ae8559d7
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-26 12:51:41 +0100

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,7 +1,8 @@
 set -e
-CC=clang  CXX=clang++ CFLAGS=-fsanitize=address 
+CC=clang  CXX=clang++ CFLAGS=-fsanitize=address FUZZING_ENGINE=afl
 export CC CXX
 set $LLVM_CONFIG=/usr/bin/llvm-config-10
+export FUZZING_ENGINE=afl
 
 
 cd postfix
@@ -11,10 +12,10 @@ BASE=$PWD
 
 # Compile fuzzers
 cd ${BASE}/src/global
-$CC $CFLAGS -DHAS_DEV_URANDOM -DSNAPSHOT -UUSE_DYNAMIC_LIBS -DDEF_SHLIB_DIR=\"no\" \
+$CC $CFLAGS $LIB_FUZZING_ENGINE -DHAS_DEV_URANDOM -DSNAPSHOT -UUSE_DYNAMIC_LIBS -DDEF_SHLIB_DIR=\"no\" \
                -UUSE_DYNAMIC_MAPS -I. -I../../include -DNO_EAI -DDEF_SMTPUTF8_ENABLE=\"no\" \
                 -g -O -DLINUX4 -Wformat -Wno-comment -fno-common -c $SRC/fuzz_tok822.c
-$CC $CFLAGS -DHAS_DEV_URANDOM -DSNAPSHOT -UUSE_DYNAMIC_LIBS -DDEF_SHLIB_DIR=\"no\" \
+$CC $CFLAGS $LIB_FUZZING_ENGINE -DHAS_DEV_URANDOM -DSNAPSHOT -UUSE_DYNAMIC_LIBS -DDEF_SHLIB_DIR=\"no\" \
                -UUSE_DYNAMIC_MAPS -I. -I../../include -DNO_EAI -DDEF_SMTPUTF8_ENABLE=\"no\" \
                 -g -O -DLINUX4 -Wformat -Wno-comment -fno-common -c $SRC/fuzz_mime.c
 
