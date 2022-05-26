54068e2c5158758606525ee9b6193d2a8428107b
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-26 12:00:13 +0530

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -2,6 +2,7 @@ set -e
 CC=afl-clang-fast CXX=afl-clang-fast++ CFLAGS=-fsanitize=address
 
 set $LLVM_CONFIG=/usr/bin/llvm-config-10
+set $gcc=/usr/bin/gcc.exe /usr/lib/gcc /usr/share/man/man1/gcc.1.gz
 
 export AFL_SKIP_OSSFUZZ=1
 export AFL_LLVM_INSTRUMENT=CLASSIC
@@ -11,8 +12,6 @@ export CC CXX
 rm -rf afl-build
 git clone --depth=1 https://github.com/AFLplusplus/AFLplusplus afl-build
 cd afl-build
-set $AFL_CC=gcc
-make
 
 make source-only
 ar ru FuzzingEngine.a afl-compiler-rt.o utils/aflpp_driver/aflpp_driver.o
@@ -29,16 +28,7 @@ $CC -fsanitize=address fuzz_tok822.c FuzzingEngine.a -o fuzzer
 # Test out the build by fuzzing it. INPUT_CORPUS is a directory containing files. Ctrl-C when done.
 AFL_SKIP_CPUFREQ=1 ./afl-fuzz -i $INPUT_CORPUS -o output -m none ./fuzzer
 # Create a fuzzer build to upload to ClusterFuzz.
-zip fuzzer-build.zip fuzzer afl-fuzz afl-showmap
-
-
-
-  
-  
-  
-  
-  
-  
+zip fuzzer-build.zip fuzzer afl-fuzz afl-showmap 
   
   
   
